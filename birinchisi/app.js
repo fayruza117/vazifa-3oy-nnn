22aa535baf3bf39ba2505b21dd9a3be0fc5d91b9
fayruza117 <fbobomurodova073@gmail.com>
2023-11-08 15:42:04 +0300

    yangila

---
+++ birinchisi/app.js
@@ -0,0 +1,9 @@
+
+confirm("Malumotingizni kirirting");
+let fname=prompt("isming nima?");
+let student = prompt("qayerda o'qiysan ?");
+let age = prompt("yoshing nechida ?");
+let home = prompt("kim bo'lmoqchisan?");
+let ismarried = prompt("turmush qurganmisan?");
+let married = prompt("nechta farzanding bor")
+console.log("mening ismim" +" " + fname + " ", "men" + " " +  student + "da o'qiyman", "yoshim"  + " " + age + " "+ "da", "men" + " " + home + " " + "bo'lmoqciman" + "men xali" + ismarried + " lekin" + married +"bor" );
